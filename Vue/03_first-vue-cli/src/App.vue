3645d3d4a0b02356e38adee59ded4e8d3072db0e
dabini <jdb960211@gmail.com>
2020-05-27 12:52:36 +0900

    vue pjt

---
+++ Vue/03_first-vue-cli/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyComponent />
+    <GreatComponent />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'
+import GreatComponent from './components/GreatComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent,
+    GreatComponent,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
