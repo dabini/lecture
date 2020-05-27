e0d4b26cc41f245a25d1f169102b92836ccc4067
dabini <jdb960211@gmail.com>
2020-05-27 13:45:37 +0900

    vue - lotto/ lunch random

---
+++ Vue/exercise_0527/src/components/Lotto.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Lotto</h2>
+    <button @click="getLotto">랜덤선택</button>
+    <p>{{ lottoNumber }}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+    data : function() {
+        return {
+            numbers : _.range(1, 46),
+            lottoNumber : '',
+        }
+    },
+    methods : {
+        getLotto() {
+            this.lottoNumber = _.sampleSize(this.numbers, 6)
+            this.lottoNumber.sort((a, b) => a-b)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/exercise_0527/src/components/Lunch.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>Lunch</h2>
+        <!-- <p>{{ menus }}</p> -->
+        <button @click="pick">랜덤선택</button>
+        <p>{{ choice }}</p>
+    </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+    data : function() {
+        return {
+            menus : ['짜장면', '짬뽕', '김밥', '돈까스', '피자', '라면', '제육볶음'],
+            choice: ''
+        }
+    },
+    methods : {
+        pick : function() {
+            this.choice = _.sample(this.menus)
+        }
+    }
+
+}
+</script>
+
+<style>
+    
+</style>
